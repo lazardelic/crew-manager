cb8748bb2b30521065ac550df623deac5950f492
Lucky <lazar.delic@helbiz.com>
2020-08-21 12:07:25 +0200

    ADD: vue3 setup

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./registerServiceWorker";
+import { createApp } from 'vue'
+import App from './App.vue'
+import './index.css'
 
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount("#app");
+createApp(App).mount('#app')
